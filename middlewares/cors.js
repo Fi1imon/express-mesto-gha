c3e5019d979da3ebf1aa5f7d1e88d7647b688709
Fi1imon <angelo4ekfilippof@gmail.com>
2023-09-29 00:31:01 +0300

    дебаг cors

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,7 +1,6 @@
 const allowedCors = [
   'https://mesto.fi1imon.nomoredomainsicu.ru',
   'https://mesto.fi1imon.nomoredomainsicu.ru/',
-  'http://localhost:3000/',
 ];
 
 const checkCors = (req, res, next) => {
@@ -11,16 +10,16 @@ const checkCors = (req, res, next) => {
 
   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
 
-  if (method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-    res.header('Access-Control-Allow-Headers', requestHeaders);
-
-    res.end();
-    return;
-  }
-
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
+
+    if (method === 'OPTIONS') {
+      res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+      res.header('Access-Control-Allow-Headers', requestHeaders);
+
+      res.end();
+      return;
+    }
   }
 
   next();
