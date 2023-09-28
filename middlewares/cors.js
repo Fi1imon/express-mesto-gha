6ea9ee44e8ffc2115c27537035cc630c0207aae8
Fi1imon <angelo4ekfilippof@gmail.com>
2023-09-29 01:26:27 +0300

    дебаг cors

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -11,6 +11,7 @@ const checkCors = (req, res, next) => {
   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
 
   if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Credentials', 'true');
     res.header('Access-Control-Allow-Origin', origin);
 
     if (method === 'OPTIONS') {
